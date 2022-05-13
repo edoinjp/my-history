14b3bdc829ac8b6542457b06f1abcf337e2ef1a5
edoinjp <edoinjp@gmail.com>
2022-05-13 19:15:10 +0900

    text add

---
+++ script.js
@@ -0,0 +1,6 @@
+let navigation = document.getElementById("navv")
+let burgeticon = document.getElementById("burgericon")
+
+burgeticon.addEventListener("click", function(){
+    navigation.classList.toggle('ul-active')
+})
